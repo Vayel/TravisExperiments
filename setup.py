27290ae8f90cd401ca4ad73930a11d46ee25aeca
vincent <vincent.lefoulon@free.fr>
2015-02-01 11:32:30 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+import package
+
+
+
+def strip_comments(l):
+    return l.split('#', 1)[0].strip()
+
+def reqs(*f):
+    return list(filter(None, [strip_comments(l) for l in open(os.path.join(os.getcwd(), *f)).readlines()]))
+
+
+install_requires = reqs("requirements.txt")
+
+packages = ["package"]
+
+classifiers = [
+    "Programming Language :: Python"
+]
+
+setup(
+    name="package",
+    packages=packages,
+    author="Vayel",
+    description="",
+    long_description="",
+    license="MIT",
+    classifiers=classifiers,
+    install_requires=install_requires,
+    include_package_data=True
+)
