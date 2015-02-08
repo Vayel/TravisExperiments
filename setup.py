42e429ec3f9a66fbdd9a1dc4bf50afa56d478b7a
vincent <vincent.lefoulon@free.fr>
2015-02-08 17:40:29 +0100

    Clean setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
-import os
 from setuptools import setup
 
 import package
 
 
 
-here = os.path.dirname(os.path.realpath(__file__))
-
 setup(
     name="package",
-    version="0.0.1",
-    description="",
-    long_description="",
-    author="Vayel",
-    author_email="vincent.lefoulon@free.fr",
-    url="https://github.com/Vayel/TravisExperiments",
     packages=["package"],
-    include_package_data=True,
-    license="MIT"
 )
